61ccc446b6f4e4f88e1e155c068e24315c6fc68c
Samar1006 <samarkhapre@gmail.com>
2025-04-26 01:14:43 -0700

    graph working kind of

--- graph.js
+++ graph.js
@@ -1,11 +1,52 @@
-const logo = document.getElementById('logo');
-const packs = document.getElementById('packs');
+const cigs = document.getElementById("question1");
+let num = 0;
 
-packs.addEventListener('input', () => {
-	const number = parseFloat(packs.value);
-	console.log(number);
+const ctx = document.getElementById('graph').getContext('2d');
+
+const myChart = new Chart(ctx, {
+	type: 'line',
+	data: {
+		datasets: [{
+			label: 'Simple Dataset',
+			borderWidth: 1
+		}]
+	},
+	options: {
+		responsive: true,
+		plugins: {
+			title: {
+				display: true,
+				text: '',
+				font: {
+					size: 24
+				}
+			}
+		},
+		scales: {
+			x: {
+				ticks: {
+					maxTicksLimit: 5
+				}
+			},
+			y: {
+				ticks: {
+					maxTicksLimit: 5
+				}
+			}
+		}
+	}
 });
 
-document.getElementById('logo').addEventListener('click', function() {
-	window.location.href = 'front.html';
+cigs.addEventListener("keydown", function(event) {
+	if (event.key === "Enter") {
+		num = cigs.value;
+		console.log(num);
+		myChart.options.plugins.title.text = `At ${num} cigarettes per day`;
+
+		myChart.data.datasets[0].data.push({ x: 1, y: Number(num) });
+
+		myChart.update();
+
+
+	}
 });
