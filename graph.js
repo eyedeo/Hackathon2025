c124063c64e26d84f73c898e4a5caaa7ad70d0f5
Ido S. Aseo <ido.s.aseo@gmail.com>
2025-04-26 00:03:02 -0700

    making vape mode compatible with yall code

--- graph.js
+++ graph.js
@@ -1,5 +1,6 @@
 const logo = document.getElementById('logo');
 const packs = document.getElementById('packs');
+const qs = document.getElementsByClassName('questions');
 
 packs.addEventListener('input', () => {
 	const number = parseFloat(packs.value);
@@ -9,3 +10,23 @@ packs.addEventListener('input', () => {
 logo.addEventListener('click', () => {
 	window.location.href = "";
 });
+
+const toggle = document.getElementById('mode-toggle');
+const label = document.getElementById('mode-label');
+
+toggle.addEventListener('change', () => {
+	label.textContent = toggle.checked ? 'Vape Mode' : 'Cig Mode';
+	if (toggle.checked) {
+		document.body.classList.add('vape-mode');
+		logo.src='resources/nonico.png';
+		qs[0].textContent = 'How many vape pens do you go through per year?';
+		qs[1].textContent = 'How much does a usual vape pen cost?';
+
+	} else {
+		document.body.classList.remove('vape-mode');
+		logo.src='resources/nblogo.png';
+		qs[0].textContent = 'How many cigarettes do you smoke a day?';
+		qs[1].textContent = 'How much does a pack of cigarettes cost?';
+	}
+});
+
